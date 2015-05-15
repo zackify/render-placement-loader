59e99856b36e70588ebf1fa8592a9e329bab5220
Jimmy Jia <jjia@4catalyzer.com>
2015-05-15 02:03:48 -0400

    Hackishly load exported component

--- index.js
+++ index.js
@@ -4,19 +4,15 @@ module.exports = function(source) {
 	var query = loaderUtils.parseQuery(this.query);
 	var props = query.props || {};
 
-	//if it's an es6 class
-	if(source.match(/\bclass\s(\w+)/)) var className = source.match(/\bclass\s(\w+)/)[1]
-	//if it's an old school component
-	else if(source.match(/var (.*) =/)) var className = source.match(/var (.*) =/)[1]
-
-	var render = 'React.render(React.createElement(' + className + ', ' + JSON.stringify(props) + '), document.body)'
+	var clazz = 'exports.__esModule ? exports.default : module.exports';
+	var render = 'React.render(React.createElement(' + clazz + ', ' + JSON.stringify(props) + '), document.body)';
 
 	//if replace a react render is not set to false or if there is no react render, let's add it
 	if(query.replace !== 'false' || !source.match(/\bReact.render\((.*)\)/,'')){
-		source = source.replace(/\bReact.render\((.*)\)/,'')
-		return source + render
+		source = source.replace(/\bReact.render\((.*)\)/,'');
+		return source + render;
 	}
 	else {
-		return source
+		return source;
 	}
-}
+};
